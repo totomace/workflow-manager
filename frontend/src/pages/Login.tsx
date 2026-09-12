e27817c07c18f296ee9f781c92129838090ba22b
Khanh-Kiet <khanhkietbt2020@gmail.com>
2026-09-12 14:19:39 +0700

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook_form';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { GoogleLogin } from '@react-oauth/google';
 import { motion } from 'framer-motion';
